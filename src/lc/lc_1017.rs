eb85be93b25f41ba533d2624bb69ea0b06e8b7b2
Yongxin Wang <fefe82@qq.com>
2023-04-06 10:18:51 +0800

    1017. Convert to Base -2

---
+++ src/lc/lc_1017.rs
@@ -0,0 +1,30 @@
+// https://leetcode.cn/problems/convert-to-base-2/
+// 1017. Convert to Base -2
+pub struct Solution;
+impl Solution {
+    pub fn base_neg2(mut n: i32) -> String {
+        if n == 0 {
+            return String::from("0");
+        }
+        let mut repr = Vec::new();
+        let mut idx = 0;
+        while n > 0 {
+            repr.push(b'0' + (n % 2) as u8);
+            if n % 2 != 0 && idx % 2 != 0 {
+                n += 2;
+            }
+            n /= 2;
+            idx += 1;
+        }
+        String::from_utf8(repr.into_iter().rev().collect()).unwrap()
+    }
+}
+mod tests {
+    use super::*;
+    #[test]
+    fn base_neg2() {
+        assert_eq!(Solution::base_neg2(2), String::from("110"));
+        assert_eq!(Solution::base_neg2(3), String::from("111"));
+        assert_eq!(Solution::base_neg2(4), String::from("100"));
+    }
+}
--- src/lc/mod.rs
+++ src/lc/mod.rs
@@ -5,6 +5,7 @@ pub mod lc_1000;
 pub mod lc_101;
 pub mod lc_1011;
 pub mod lc_1012;
+pub mod lc_1017;
 pub mod lc_1032;
 pub mod lc_1039;
 pub mod lc_1053;
