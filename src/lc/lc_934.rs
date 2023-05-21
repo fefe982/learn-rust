add6c891918ec5f88fb2a13357c04922531c9795
Yongxin Wang <fefe82@qq.com>
2023-05-21 09:14:55 +0800

    934. Shortest Bridge

---
+++ src/lc/lc_934.rs
@@ -0,0 +1,80 @@
+// https://leetcode.com/problems/shortest-bridge/
+// 934. Shortest Bridge
+pub struct Solution;
+impl Solution {
+    pub fn shortest_bridge(mut grid: Vec<Vec<i32>>) -> i32 {
+        let n = grid.len();
+        let dir = [[0, 1], [1, 0], [-1, 0], [0, -1]];
+        let mut q1 = std::collections::VecDeque::new();
+        let mut qn = std::collections::VecDeque::new();
+        'find: for i in 0..n {
+            for j in 0..n {
+                if grid[i][j] == 1 {
+                    q1.push_back((i, j));
+                    break 'find;
+                }
+            }
+        }
+        while let Some((x, y)) = q1.pop_front() {
+            if grid[x][y] != 1 {
+                continue;
+            }
+            grid[x][y] = -1;
+            for i in 0..4 {
+                let nx = (x as i32 + dir[i][0]) as usize;
+                let ny = (y as i32 + dir[i][1]) as usize;
+                if nx < n && ny < n {
+                    if grid[nx][ny] == 1 {
+                        q1.push_back((nx, ny));
+                    } else if grid[nx][ny] == 0 {
+                        grid[nx][ny] = 2;
+                        qn.push_back((nx, ny));
+                    }
+                }
+            }
+        }
+        while let Some((x, y)) = qn.pop_front() {
+            if grid[x][y] < 0 {
+                continue;
+            }
+            let c = grid[x][y];
+            grid[x][y] = -1;
+            for i in 0..4 {
+                let nx = (x as i32 + dir[i][0]) as usize;
+                let ny = (y as i32 + dir[i][1]) as usize;
+                if nx < n && ny < n {
+                    if grid[nx][ny] == 1 {
+                        return c - 1;
+                    } else if grid[nx][ny] == 0 {
+                        grid[nx][ny] = c + 1;
+                        qn.push_back((nx, ny));
+                    }
+                }
+            }
+        }
+        0
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::*;
+    #[test]
+    fn shortest_bridge() {
+        assert_eq!(Solution::shortest_bridge(vec_vec![[0, 1], [1, 0]]), 1);
+        assert_eq!(
+            Solution::shortest_bridge(vec_vec![[0, 1, 0], [0, 0, 0], [0, 0, 1]]),
+            2
+        );
+        assert_eq!(
+            Solution::shortest_bridge(vec_vec![
+                [1, 1, 1, 1, 1],
+                [1, 0, 0, 0, 1],
+                [1, 0, 1, 0, 1],
+                [1, 0, 0, 0, 1],
+                [1, 1, 1, 1, 1]
+            ]),
+            1
+        );
+    }
+}
--- src/lc/mod.rs
+++ src/lc/mod.rs
@@ -227,6 +227,7 @@ pub mod lc_875;
 pub mod lc_879;
 pub mod lc_881;
 pub mod lc_912;
+pub mod lc_934;
 pub mod lc_938;
 pub mod lc_946;
 pub mod lc_954;
