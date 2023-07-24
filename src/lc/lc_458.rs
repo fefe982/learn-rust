16e8a8ec9f5ad794938f4ceab5c28b8aabb01332
Yongxin Wang <fefe82@qq.com>
2023-07-24 13:28:43 +0800

    precision problem

--- src/lc/lc_458.rs
+++ src/lc/lc_458.rs
@@ -3,7 +3,7 @@
 pub struct Solution;
 impl Solution {
     pub fn poor_pigs(buckets: i32, minutes_to_die: i32, minutes_to_test: i32) -> i32 {
-        ((buckets as f64).ln() / ((minutes_to_test / minutes_to_die + 1) as f64).ln()).ceil() as i32
+        ((buckets as f32).ln() / ((minutes_to_test / minutes_to_die + 1) as f32).ln()).ceil() as i32
     }
 }
 #[cfg(test)]
@@ -11,6 +11,7 @@ mod tests {
     use super::*;
     #[test]
     fn poor_pigs() {
+        assert_eq!(Solution::poor_pigs(125, 1, 4), 3);
         assert_eq!(Solution::poor_pigs(4, 15, 15), 2);
         assert_eq!(Solution::poor_pigs(4, 15, 30), 2);
     }
