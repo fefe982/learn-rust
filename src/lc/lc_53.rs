5a3233a27ae35d9666747d0ec7a3ba1726622a50
Yongxin Wang <fefe82@qq.com>
2023-11-20 10:06:50 +0800

    53. Maximum Subarray

--- src/lc/lc_53.rs
+++ src/lc/lc_53.rs
@@ -4,7 +4,7 @@ pub struct Solution;
 impl Solution {
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
         nums.into_iter()
-            .fold((0, 0), |(max, acc), x| {
+            .fold((i32::MIN, 0), |(max, acc), x| {
                 let nacc = x.max(acc + x);
                 (max.max(nacc), nacc)
             })
@@ -16,6 +16,7 @@ mod tests {
     use super::*;
     #[test]
     fn test_max_sub_array() {
+        assert_eq!(Solution::max_sub_array(vec![-1]), -1);
         assert_eq!(Solution::max_sub_array(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
         assert_eq!(Solution::max_sub_array(vec![1]), 1);
         assert_eq!(Solution::max_sub_array(vec![5, 4, -1, 7, 8]), 23);
