415ad226e6b0a117b8bbd4138995158b01abffc5
Yongxin Wang <fefe82@qq.com>
2023-12-04 10:46:19 +0800

    780. Reaching Points

---
+++ src/lc/lc_780.rs
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/reaching-points/
+// 780. Reaching Points
+pub struct Solution;
+impl Solution {
+    pub fn reaching_points(sx: i32, sy: i32, mut tx: i32, mut ty: i32) -> bool {
+        loop {
+            if tx < sx {
+                return false;
+            }
+            if sx == tx && sy <= ty && sy % sx == ty % tx {
+                return true;
+            }
+            ty = ty % tx;
+            if ty < sy {
+                return false;
+            }
+            if sy == ty && sx <= tx && sx % sy == tx % ty {
+                return true;
+            }
+            tx = tx % ty;
+        }
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_reaching_points() {
+        assert_eq!(Solution::reaching_points(3, 7, 3, 4), false);
+        assert_eq!(Solution::reaching_points(1, 1, 3, 5), true);
+        assert_eq!(Solution::reaching_points(1, 1, 2, 2), false);
+        assert_eq!(Solution::reaching_points(1, 1, 1, 1), true);
+    }
+}
--- src/lc/mod.rs
+++ src/lc/mod.rs
@@ -572,6 +572,7 @@ pub mod lc_771;
 pub mod lc_773;
 pub mod lc_778;
 pub mod lc_779;
+pub mod lc_780;
 pub mod lc_785;
 pub mod lc_799;
 pub mod lc_804;
