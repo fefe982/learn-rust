327414ef2ce90793afe9ee59cff2893c744579e5
Yongxin Wang <fefe82@qq.com>
2024-01-14 09:30:37 +0800

    952. Largest Component Size by Common Factor

--- src/lc/lc_952.rs
+++ src/lc/lc_952.rs
@@ -2,32 +2,6 @@
 // 952. Largest Component Size by Common Factor
 pub struct Solution;
 impl Solution {
-    fn seive(n: i32) -> Vec<i32> {
-        let mut v = vec![false; n as usize + 1];
-        let mut i = 2;
-        let mut res = vec![];
-        while i * i <= n {
-            res.push(i);
-            for j in 2.. {
-                if i * j > n {
-                    break;
-                }
-                v[(i * j) as usize] = true;
-            }
-            i += 1;
-            while i <= n && v[i as usize] {
-                i += 1;
-            }
-        }
-        while i <= n {
-            res.push(i);
-            i += 1;
-            while i <= n && v[i as usize] {
-                i += 1;
-            }
-        }
-        res
-    }
     fn getp(p: &mut Vec<(usize, i32)>, x: usize) -> usize {
         if p[x].0 == x {
             return x;
@@ -36,7 +10,11 @@ impl Solution {
         p[x].0
     }
     pub fn largest_component_size(nums: Vec<i32>) -> i32 {
-        let primes = Self::seive(317);
+        let primes = vec![
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103,
+            107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223,
+            227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317,
+        ];
         let mut parr = (0..50000).map(|i| (i, 0)).collect::<Vec<_>>();
         let mut ans = 1;
         for i in 0..nums.len() {
