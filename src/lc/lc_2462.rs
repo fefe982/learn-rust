da947920e06cfc21761e9e746d048d5790ca6059
Yongxin Wang <fefe82@qq.com>
2024-05-01 19:59:34 +0800

    2462. Total Cost to Hire K Workers

--- src/lc/lc_2462.rs
+++ src/lc/lc_2462.rs
@@ -3,33 +3,26 @@
 pub struct Solution;
 impl Solution {
     pub fn total_cost(costs: Vec<i32>, k: i32, candidates: i32) -> i64 {
-        let mut front = std::collections::BinaryHeap::new();
-        let mut back = std::collections::BinaryHeap::new();
+        let mut h = std::collections::BinaryHeap::new();
         let mut c = candidates as usize;
         let mut bc = (costs.len() - c).max(c);
         for i in 0..c {
-            front.push(-costs[i]);
+            h.push(std::cmp::Reverse((costs[i], i)));
         }
         for i in bc..costs.len() {
-            back.push(-costs[i]);
+            h.push(std::cmp::Reverse((costs[i], i)));
         }
         let mut sum = 0;
         for _ in 0..k {
-            let mf = -*front.peek().or(Some(&(i32::MIN / 2))).unwrap();
-            let mb = -*back.peek().or(Some(&(i32::MIN / 2))).unwrap();
-            if mf <= mb {
-                front.pop();
-                sum += mf as i64;
-                if c < bc {
-                    front.push(-costs[c]);
+            let std::cmp::Reverse((cost, i)) = h.pop().unwrap();
+            sum += cost as i64;
+            if c < bc {
+                if i < c {
+                    h.push(std::cmp::Reverse((costs[c], c)));
                     c += 1;
-                }
-            } else {
-                back.pop();
-                sum += mb as i64;
-                if c < bc {
+                } else {
                     bc -= 1;
-                    back.push(-costs[bc]);
+                    h.push(std::cmp::Reverse((costs[bc], bc)));
                 }
             }
         }
@@ -41,10 +34,7 @@ mod tests {
     use super::*;
     #[test]
     fn total_cost() {
-        assert_eq!(
-            Solution::total_cost(vec![17, 12, 10, 2, 7, 2, 11, 20, 8], 3, 4),
-            11
-        );
+        assert_eq!(Solution::total_cost(vec![17, 12, 10, 2, 7, 2, 11, 20, 8], 3, 4), 11);
         assert_eq!(Solution::total_cost(vec![1, 2, 4, 1], 3, 3), 4);
     }
 }
