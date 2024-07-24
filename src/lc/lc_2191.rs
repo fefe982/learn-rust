9e715c45a7aef5e0d036b2fa3875433f4a4e36ae
Yongxin Wang <fefe82@qq.com>
2024-07-24 09:38:12 +0800

    2191. Sort the Jumbled Numbers

--- src/lc/lc_2191.rs
+++ src/lc/lc_2191.rs
@@ -3,6 +3,9 @@
 pub struct Solution;
 impl Solution {
     fn map(num: i32, mapping: &Vec<i32>) -> i32 {
+        if num == 0 {
+            return mapping[0];
+        }
         let mut n = 0;
         let mut num = num;
         let mut power = 1;
@@ -28,6 +31,10 @@ mod tests {
     use super::*;
     #[test]
     fn test_sort_jumbled() {
+        assert_eq!(
+            Solution::sort_jumbled(vec![9, 8, 7, 6, 5, 4, 3, 2, 1, 0], vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+        );
         assert_eq!(
             Solution::sort_jumbled(vec![8, 9, 4, 0, 2, 1, 3, 5, 7, 6], vec![991, 338, 38]),
             [338, 38, 991]
