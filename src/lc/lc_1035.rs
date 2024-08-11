98527b6d035a2164e2caa6d959cea5f2aa5233ba
Yongxin Wang <fefe82@qq.com>
2024-08-11 08:05:42 +0800

    1035. Uncrossed Lines

--- src/lc/lc_1035.rs
+++ src/lc/lc_1035.rs
@@ -2,17 +2,13 @@
 // 1035. Uncrossed Lines
 pub struct Solution;
 impl Solution {
-    fn max_uncrossed_lines_slice(
-        nums1: &[i32],
-        nums2: &[i32],
-        cache: &mut std::collections::HashMap<(usize, usize), i32>,
-    ) -> i32 {
+    fn max_uncrossed_lines_slice(nums1: &[i32], nums2: &[i32], cache: &mut Vec<Vec<i32>>) -> i32 {
         if nums1.len() == 0 || nums2.len() == 0 {
             return 0;
         }
-        let l = (nums1.len(), nums2.len());
-        if let Some(&i) = cache.get(&l) {
-            return i;
+        let c = cache[nums1.len()][nums2.len()];
+        if c != -1 {
+            return c;
         }
         let cnt = if nums1[0] == nums2[0] {
             Self::max_uncrossed_lines_slice(&nums1[1..], &nums2[1..], cache) + 1
@@ -22,14 +18,14 @@ impl Solution {
                 Self::max_uncrossed_lines_slice(nums1, &nums2[1..], cache),
             )
         };
-        cache.insert(l, cnt);
+        cache[nums1.len()][nums2.len()] = cnt;
         cnt
     }
     pub fn max_uncrossed_lines(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
         Self::max_uncrossed_lines_slice(
             &nums1[..],
             &nums2[..],
-            &mut std::collections::HashMap::new(),
+            &mut vec![vec![-1; nums2.len() + 1]; nums1.len() + 1],
         )
     }
 }
@@ -38,10 +34,7 @@ mod tests {
     use super::*;
     #[test]
     fn max_uncrossed_lines() {
-        assert_eq!(
-            Solution::max_uncrossed_lines(vec![1, 4, 2], vec![1, 2, 4]),
-            2
-        );
+        assert_eq!(Solution::max_uncrossed_lines(vec![1, 4, 2], vec![1, 2, 4]), 2);
         assert_eq!(
             Solution::max_uncrossed_lines(vec![2, 5, 1, 2, 5], vec![10, 5, 2, 1, 5, 2]),
             3
