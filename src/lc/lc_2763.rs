c4782efb0d8da33b6ffeb9d8644a54c4e14060a4
Yongxin Wang <fefe82@qq.com>
2024-09-13 10:04:29 +0800

    2763. Sum of Imbalance Numbers of All Subarrays

--- src/lc/lc_2763.rs
+++ src/lc/lc_2763.rs
@@ -5,40 +5,20 @@ impl Solution {
     pub fn sum_imbalance_numbers(nums: Vec<i32>) -> i32 {
         let mut ans = 0;
         for i in 0..nums.len() {
-            let mut set = std::collections::BTreeSet::new();
-            set.insert(nums[i]);
-            let mut imb = 0;
-            for j in i + 1..nums.len() {
-                let nj = nums[j];
-                if !set.contains(&nj) {
-                    let prev = set.range(..nj).last();
-                    let next = set.range(nj..).next();
-                    match (prev, next) {
-                        (Some(p), Some(n)) => {
-                            if nj - p > 1 {
-                                imb += 1;
-                            }
-                            if n - nj > 1 {
-                                imb += 1;
-                            }
-                            if n - p > 1 {
-                                imb -= 1;
-                            }
-                        }
-                        (Some(p), None) => {
-                            if nj - p > 1 {
-                                imb += 1;
-                            }
-                        }
-                        (None, Some(n)) => {
-                            if n - nj > 1 {
-                                imb += 1;
-                            }
-                        }
-                        _ => (),
+            let mut set = vec![false; nums.len() + 2];
+            let mut imb = -1;
+            for j in i..nums.len() {
+                let nj = nums[j] as usize;
+                if !set[nj] {
+                    imb += 1;
+                    if set[nj - 1] {
+                        imb -= 1;
+                    }
+                    if set[nj + 1] {
+                        imb -= 1;
                     }
                 }
-                set.insert(nj);
+                set[nj] = true;
                 ans += imb;
             }
         }
