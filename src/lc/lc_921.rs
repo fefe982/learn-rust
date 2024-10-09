141f810a41f295853bba8d7b53732ce08a98a542
Yongxin Wang <fefe82@qq.com>
2024-10-09 09:33:24 +0800

    921. Minimum Add to Make Parentheses Valid

--- src/lc/lc_921.rs
+++ src/lc/lc_921.rs
@@ -1,50 +1,27 @@
-// https://leetcode.com/problems/sort-an-array/
-// 912. Sort an Array
+// https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+// 921. Minimum Add to Make Parentheses Valid
 pub struct Solution;
 impl Solution {
-    fn sort(nums: &mut [i32]) {
-        if nums.len() <= 1 {
-            return;
-        }
-        let p = nums[0];
-        let mut l = 0;
-        let mut r = nums.len() - 1;
-        while l < r {
-            while l < r && nums[r] >= p {
-                r -= 1;
-            }
-            if l < r {
-                nums[l] = nums[r];
-            }
-            while l < r && nums[l] <= p {
-                l += 1;
-            }
-            if l < r {
-                nums[r] = nums[l];
+    pub fn min_add_to_make_valid(s: String) -> i32 {
+        let mut cnt = 0;
+        let mut min = 0;
+        for c in s.chars() {
+            if c == '(' {
+                cnt += 1;
+            } else {
+                cnt -= 1;
+                min = min.min(cnt);
             }
         }
-        nums[l] = p;
-        while l > 0 && nums[l] == p {
-            l -= 1;
-        }
-        while r < nums.len() - 1 && nums[r] == p {
-            r += 1;
-        }
-        Self::sort(&mut nums[0..=l]);
-        Self::sort(&mut nums[r..]);
-    }
-    pub fn sort_array(nums: Vec<i32>) -> Vec<i32> {
-        let mut nums = nums;
-        Self::sort(&mut nums[..]);
-        nums
+        cnt - 2 * min
     }
 }
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_sort_array() {
-        assert_eq!(Solution::sort_array(vec![5, 2, 3, 1]), [1, 2, 3, 5]);
-        assert_eq!(Solution::sort_array(vec![5, 1, 1, 2, 0, 0]), [0, 0, 1, 1, 2, 5]);
+    fn test_min_add_to_make_valid() {
+        assert_eq!(Solution::min_add_to_make_valid("())".to_string()), 1);
+        assert_eq!(Solution::min_add_to_make_valid("(((".to_string()), 3);
     }
 }
