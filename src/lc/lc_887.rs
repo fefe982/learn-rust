d8689fe94f5390ee0de273ecae9fc3b60592b855
Yongxin Wang <fefe82@qq.com>
2024-10-14 09:23:28 +0800

    887. Super Egg Drop

--- src/lc/lc_887.rs
+++ src/lc/lc_887.rs
@@ -2,38 +2,22 @@
 // 887. Super Egg Drop
 pub struct Solution;
 impl Solution {
-    fn drop(k: i32, n: i32, cache: &mut std::collections::HashMap<(i32, i32), i32>) -> i32 {
-        if k == 1 {
-            return n;
-        }
-        if n == 0 {
-            return 0;
-        }
+    pub fn super_egg_drop(k: i32, n: i32) -> i32 {
         if n == 1 {
             return 1;
         }
-        if let Some(&v) = cache.get(&(k, n)) {
-            return v;
-        }
-        let mut l = 1;
-        let mut r = n;
-        while l < r {
-            let m = (l + r) / 2;
-            let broken = Self::drop(k - 1, m - 1, cache);
-            let not_broken = Self::drop(k, n - m, cache);
-            if broken >= not_broken {
-                r = m;
-            } else {
-                l = m + 1;
+        let k = k as usize;
+        let mut f = vec![1; k + 1];
+        f[0] = 0;
+        for cnt in 2.. {
+            for i in (1..=k).rev() {
+                f[i] += f[i - 1] + 1;
+            }
+            if f[k] >= n {
+                return cnt;
             }
         }
-        let a = Self::drop(k - 1, l - 1, cache) + 1;
-        cache.insert((k, n), a);
-        a
-    }
-    pub fn super_egg_drop(k: i32, n: i32) -> i32 {
-        let mut cache = std::collections::HashMap::new();
-        Self::drop(k, n, &mut cache)
+        unreachable!()
     }
 }
 #[cfg(test)]
