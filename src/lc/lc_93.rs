6995aa4633a6f5250341b304788d4b5c305ccb44
Yongxin Wang <fefe82@qq.com>
2025-01-26 06:07:34 +0800

    93. Restore IP Addresses

--- src/lc/lc_93.rs
+++ src/lc/lc_93.rs
@@ -2,7 +2,7 @@
 // 93. Restore IP Addresses
 pub struct Solution;
 impl Solution {
-    fn restore(s: &[u8], ip: &mut Vec<char>, split: i32, last: u8, lastn: i32, res: &mut Vec<String>) {
+    fn restore(s: &[u8], ip: &mut Vec<char>, split: i32, lastn: i32, res: &mut Vec<String>) {
         if s.is_empty() {
             if split == 3 {
                 res.push(ip.iter().collect());
@@ -11,7 +11,7 @@ impl Solution {
         }
         if lastn != 0 && (lastn < 25 || (lastn == 25 && s[0] <= b'5')) {
             ip.push(s[0] as char);
-            Self::restore(&s[1..], ip, split, s[0], lastn * 10 + (s[0] - b'0') as i32, res);
+            Self::restore(&s[1..], ip, split, lastn * 10 + (s[0] - b'0') as i32, res);
             ip.pop();
         }
         if split == 3 {
@@ -19,21 +19,14 @@ impl Solution {
         }
         ip.push('.');
         ip.push(s[0] as char);
-        Self::restore(&s[1..], ip, split + 1, s[0], (s[0] - b'0') as i32, res);
+        Self::restore(&s[1..], ip, split + 1, (s[0] - b'0') as i32, res);
         ip.pop();
         ip.pop();
     }
     pub fn restore_ip_addresses(s: String) -> Vec<String> {
         let s = s.as_bytes();
         let mut res = vec![];
-        Self::restore(
-            &s[1..],
-            &mut vec![s[0] as char],
-            0,
-            s[0],
-            (s[0] - b'0') as i32,
-            &mut res,
-        );
+        Self::restore(&s[1..], &mut vec![s[0] as char], 0, (s[0] - b'0') as i32, &mut res);
         res
     }
 }
