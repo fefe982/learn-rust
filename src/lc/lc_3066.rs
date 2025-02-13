e2712de8800f11c690913076dd440a6aaa6e7619
Yongxin Wang <fefe82@qq.com>
2025-02-13 09:22:06 +0800

    3066. Minimum Operations to Exceed Threshold Value II

--- src/lc/lc_3066.rs
+++ src/lc/lc_3066.rs
@@ -4,28 +4,36 @@ pub struct Solution;
 impl Solution {
     pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
         let mut h = std::collections::BinaryHeap::new();
+        let k = k as u32;
+        let mut min = k;
+        let mut cnthigh = 0;
         for n in nums {
-            if n < k {
-                h.push(std::cmp::Reverse(n as i64));
+            let n = n as u32;
+            if 3 * n < k {
+                h.push(std::cmp::Reverse(n));
+            } else if n < k {
+                min = min.min(n);
+                cnthigh += 1;
             }
         }
         let mut cnt = 0;
-        let k = k as i64;
-        loop {
-            if let Some(std::cmp::Reverse(n1)) = h.pop() {
-                cnt += 1;
-                if let Some(std::cmp::Reverse(n2)) = h.pop() {
-                    let s = n1 * 2 + n2;
-                    if s < k {
-                        h.push(std::cmp::Reverse(s));
-                    }
-                } else {
-                    return cnt;
+        while let Some(std::cmp::Reverse(n1)) = h.pop() {
+            cnt += 1;
+            if let Some(std::cmp::Reverse(n2)) = h.pop() {
+                let s = n1 * 2 + n2;
+                if 3 * s < k {
+                    h.push(std::cmp::Reverse(s));
+                } else if s < k {
+                    min = min.min(s);
+                    cnthigh += 1;
                 }
             } else {
-                return cnt;
+                if n1 * 2 + min >= k {
+                    cnthigh -= 1;
+                }
             }
         }
+        cnt + (cnthigh + 1) / 2
     }
 }
 #[cfg(test)]
@@ -33,6 +41,10 @@ mod tests {
     use super::*;
     #[test]
     fn test_min_operations() {
+        assert_eq!(
+            Solution::min_operations(vec![999999999, 999999999, 999999999], 1000000000),
+            2
+        );
         assert_eq!(Solution::min_operations(vec![2, 11, 10, 1, 3], 10), 2);
         assert_eq!(Solution::min_operations(vec![1, 1, 2, 4, 9], 20), 4);
     }
