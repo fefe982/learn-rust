073c8bba62bb1943d3be61cd1baa51a1c831aa7b
Yongxin Wang <fefe82@qq.com>
2025-02-21 08:54:10 +0800

    217. Contains Duplicate

---
+++ src/lc/lc_217.rs
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/contains-duplicate/
+// 217. Contains Duplicate
+pub struct Solution;
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut set = std::collections::HashSet::with_capacity(nums.len());
+        for n in nums {
+            if !set.insert(n) {
+                return true;
+            }
+        }
+        false
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn contains_duplicate() {
+        assert_eq!(Solution::contains_duplicate(vec![1, 2, 3, 1]), true);
+        assert_eq!(Solution::contains_duplicate(vec![1, 2, 3, 4]), false);
+        assert_eq!(Solution::contains_duplicate(vec![1, 1, 1, 3, 3, 4, 3, 2, 4, 2]), true);
+    }
+}
