31705834cc3d602cd9a1e5decff4ff3f5a1b05e8
Yongxin Wang <fefe82@qq.com>
2026-02-16 09:01:40 +0800

    190. Reverse Bits

--- src/lc/lc01/lc_190.rs
+++ src/lc/lc01/lc_190.rs
@@ -2,7 +2,7 @@
 // 190. Reverse Bits
 pub struct Solution;
 impl Solution {
-    pub fn reverse_bits(x: u32) -> u32 {
+    pub fn reverse_bits(x: i32) -> i32 {
         x.reverse_bits()
     }
 }
@@ -12,6 +12,6 @@ mod tests {
     #[test]
     fn test_reverse_bits() {
         assert_eq!(Solution::reverse_bits(43261596), 964176192);
-        assert_eq!(Solution::reverse_bits(4294967293), 3221225471);
+        assert_eq!(Solution::reverse_bits(2147483644), 1073741822);
     }
 }
--- src/lc/lc01/mod.rs
+++ src/lc/lc01/mod.rs
@@ -1,3 +1,4 @@
 pub mod lc_115;
 pub mod lc_127;
 pub mod lc_132;
+pub mod lc_190;
