7ee5b8c710ef1f25c2d0b3b3cdfeb4938ee9eacf
Yongxin Wang <fefe82@qq.com>
2026-05-26 05:10:10 +0800

    3120. Count the Number of Special Characters I

---
+++ src/lc/lc31/lc_3120.rs
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/count-the-number-of-special-characters-i/
+// 3120. Count the Number of Special Characters I
+pub struct Solution;
+impl Solution {
+    pub fn number_of_special_chars(word: String) -> i32 {
+        let mut count = [0; 26];
+        for c in word.chars() {
+            if c.is_ascii_lowercase() {
+                count[(c as u8 - b'a') as usize] |= 1;
+            } else if c.is_ascii_uppercase() {
+                count[(c as u8 - b'A') as usize] |= 2;
+            }
+        }
+        count.into_iter().filter(|&c| c == 3).count() as i32
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_number_of_special_chars() {
+        assert_eq!(Solution::number_of_special_chars("aaAbcBC".to_string()), 3);
+        assert_eq!(Solution::number_of_special_chars("abc".to_string()), 0);
+        assert_eq!(Solution::number_of_special_chars("abBCab".to_string()), 1);
+    }
+}
