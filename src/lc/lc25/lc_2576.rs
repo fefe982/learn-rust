49d0caee22e2d2c69b3c5f494f32068f775cfefb
Yongxin Wang <fefe82@qq.com>
2026-07-07 05:08:13 +0800

    2576. Find the Maximum Number of Marked Indices

--- src/lc/lc25/lc_2576.rs
+++ src/lc/lc25/lc_2576.rs
@@ -2,32 +2,38 @@
 // 2576. Find the Maximum Number of Marked Indices
 pub struct Solution;
 impl Solution {
-    pub fn max_num_of_marked_indices(mut nums: Vec<i32>) -> i32 {
+    pub fn max_num_of_marked_indices(nums: Vec<i32>) -> i32 {
+        if nums.len() < 2 {
+            return 0;
+        }
+        let mut nums = nums;
         nums.sort_unstable();
-        let len = nums.len();
-        let mut h = len / 2;
-        let mut sum = 0;
-        for l in 0..len / 2 {
-            while h < len && 2 * nums[l] > nums[h] {
-                h += 1;
-            }
-            if h < len {
-                sum += 2;
-                h += 1;
-            }
-            if h >= len {
+        let mut j = nums.len() - 1;
+        let mut i = (nums.len() / 2) - 1;
+        let mut ans = 0;
+        loop {
+            if nums[i] * 2 <= nums[j] {
+                ans += 2;
+                if i == 0 {
+                    break;
+                }
+                i -= 1;
+                j -= 1;
+            } else if i == 0 {
                 break;
+            } else {
+                i -= 1;
             }
         }
-        sum
+        ans
     }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn max_num_of_marked_indices() {
+        assert_eq!(Solution::max_num_of_marked_indices(vec![36]), 0);
         assert_eq!(Solution::max_num_of_marked_indices(vec![3, 5, 2, 4]), 2);
         assert_eq!(Solution::max_num_of_marked_indices(vec![9, 2, 5, 4]), 4);
         assert_eq!(Solution::max_num_of_marked_indices(vec![7, 6, 8]), 0);
